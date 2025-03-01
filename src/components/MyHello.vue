6d449dab5904d2607faca8433d23da93e52f725d
Vlad <moscvin.2002@gmail.com>
2025-03-01 21:58:04 +0200

    first commit

---
+++ src/components/MyHello.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean,
+})
+console.log(props)
+</script>
+
+<template>
+  <div class="product">
+    <h2>sneaker:{{ title }}</h2>
+    <h3>price:{{ price }}</h3>
+    <h4>Added :{{ isAdded }}</h4>
+    <slot></slot>
+  </div>
+</template>
+<style>
+.product {
+  border: 1px blue solid;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
